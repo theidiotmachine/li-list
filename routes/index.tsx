da98d2cb49efb4e54375585c5aaadba22f34e890
theidiotmachine <dinsdt@googlemail.com>
2025-03-23 15:50:03 +0000

    not work so roll back

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,16 +5,27 @@ import App from "../islands/App.tsx";
 
 export default function Home(req: Request, ctx: RouteContext) {
 
+  /*
   useCSP((csp) => {
-    if (!csp.directives.styleSrc) {
-      //not good but gets me in the water
-      csp.directives.scriptSrc = [ 'unsafe-inline' ];
-    }
+
+    csp.directives.imgSrc?.push( 'unsafe-inline' );
+    csp.directives.imgSrc?.push( 'localhost:8000' );
+
+    csp.directives.scriptSrc = [];
+    csp.directives.scriptSrc?.push( 'unsafe-inline' );
+    csp.directives.scriptSrc?.push( 'localhost:8000' );
+
+    csp.directives.styleSrc?.push( 'localhost:8000' );
   });
+  */  
+  
 
   return <App />;
 }
 
+/*
+
 export const config: RouteConfig = {
   csp: true,
 };
+*/
