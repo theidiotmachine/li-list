35ce8b27b26e9374b7cf6de99aa5352ec7e18c96
theidiotmachine <dinsdt@googlemail.com>
2025-03-23 15:25:51 +0000

    CSP fix, some generated thing

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,20 @@
+import { useCSP } from "$fresh/runtime.ts";
+import { RouteConfig, RouteContext } from "$fresh/server.ts";
+
 import App from "../islands/App.tsx";
 
-export default function Home() {
+export default function Home(req: Request, ctx: RouteContext) {
+
+  useCSP((csp) => {
+    if (!csp.directives.styleSrc) {
+      //not good but gets me in the water
+      csp.directives.scriptSrc = [ 'unsafe-inline' ];
+    }
+  });
 
   return <App />;
 }
+
+export const config: RouteConfig = {
+  csp: true,
+};
