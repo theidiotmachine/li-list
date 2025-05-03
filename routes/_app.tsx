d20e542172f9b583768f3f14382336381854f4a6
theidiotmachine <dinsdt@googlemail.com>
2025-05-03 15:44:50 +0100

    mobile size?

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,8 +4,8 @@ export default function AppLayout({ Component }: PageProps) {
     <html>
       <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>li-list</title>
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0" />
+        <title>Legions Imperialis List Builder</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
