c483b2216b880b4461b2533eb0729932277e6b2d
theidiotmachine <dinsdt@googlemail.com>
2025-07-07 06:45:23 +0100

    mobile hello widget

--- components/HelloWidget.tsx
+++ components/HelloWidget.tsx
@@ -4,7 +4,7 @@ export function HelloWidget() {
     const {makeNewArmy} = useContext(AppState);
 
     return <div class="flex flex-row justify-center mt-48 m:mt-28">
-        <div>Make a <a onClick={()=>{makeNewArmy()}}
+        <div class="mx-3">Make a <a onClick={()=>{makeNewArmy()}}
                 class="flex-1 cursor-pointer underline">new army</a> (with a jolly generated name), or <a href='./load'
                 class="flex-1 cursor-pointer underline">load</a> a saved one.</div>
     </div>
