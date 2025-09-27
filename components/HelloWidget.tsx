6b32dfb40610acfc7e2dd0ca0ab1d15ecb34da4a
theidiotmachine <dinsdt@googlemail.com>
2025-09-27 11:57:33 +0100

    hello

--- components/HelloWidget.tsx
+++ components/HelloWidget.tsx
@@ -7,8 +7,7 @@ export function HelloWidget() {
         <div class="flex flex-col">
         <div class="mx-3">I'm working on updating the stats from the new Liber. I'm sorry to say that your existing saved armies will probably be broken.</div>
         <div class="mx-3">Current status:</div>
-        <div class="mx-3">Legions, Auxilia, Mech, Dark Mech and Titans have new points.</div>
-        <div class="mx-3">Knights do not.</div>
+        <div class="mx-3">Legions, Auxilia, Mech, Dark Mech, Titans and Knights have new points.</div>
         <div class="mx-3">No Iconic Formations yet.</div>
         <div class="mx-3 my-3"></div>
         <div class="mx-3">Make a <a onClick={()=>{makeNewArmy()}}
